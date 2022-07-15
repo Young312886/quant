c987d36fedff7f143b42d07736f237d8f57bc911
Young312 <hongym0497@gmail.com>
2022-07-15 17:15:39 +0900

    buyhold

--- buylowsellhigh.py
+++ buylowsellhigh.py
@@ -1,9 +1,10 @@
 import pandas_datareader as data
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-start_date = "2014-04-01"
-end_date = "2021-12-31"
+start_date = "2020-04-01"
+end_date = "2022-12-31"
 
 pd.set_option("display.width", 1000)
 
@@ -16,5 +17,34 @@ goog_data_signal["daily_difference"] = goog_data_signal["price"].diff()
 goog_data_signal["signal"] = 0.0
 goog_data_signal["signal"] = np.where(goog_data_signal.daily_difference > 0, 1.0, 0.0)
 
+goog_data_signal['positions'] = goog_data_signal['signal'].diff()
 
 print(goog_data_signal)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(111, ylabel="GOOG PRICE")
+goog_data_signal['price'].plot(ax = ax1, color = "r", lw=2)
+
+ax1.plot(goog_data_signal.loc[goog_data_signal.positions == -1.0].index, 
+goog_data_signal.price[goog_data_signal.positions == -1.0], 'v', markersize = 4, color = "k")
+
+ax1.plot(goog_data_signal.loc[goog_data_signal.positions == 1.0].index, 
+goog_data_signal.price[goog_data_signal.positions == 1.0], "^", markersize = 4, color = "m")
+
+#plt.show()
+
+initial_cap = float (1000.0)
+
+positions = pd.DataFrame(index = goog_data_signal.index).fillna(0.0)
+portfolio = pd.DataFrame(index=goog_data_signal.index).fillna(0.0)
+
+positions['GOOG'] = goog_data_signal['signal']
+
+portfolio['position'] = positions.multiply(goog_data_signal['price'], axis = 0)
+portfolio['cash'] = initial_cap - (positions.diff().multiply(goog_data_signal['price'], axis = 0)).cumsum()
+portfolio['total'] = portfolio['position'] + portfolio['cash']
+
+fig2 = plt.figure()
+
+portfolio.plot()
+plt.show()
